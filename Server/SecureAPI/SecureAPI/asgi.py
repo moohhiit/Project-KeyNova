a5ada1bd298010d22e43773ae8c15cdaa1e04e6a
Ansh191124 <Mransh1911@gmail.com>
2025-06-19 12:36:22 +0530

    SERVER-SIDE

--- Server/SecureAPI/SecureAPI/asgi.py
+++ Server/SecureAPI/SecureAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for contentfilter project.
+ASGI config for SecureAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contentfilter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecureAPI.settings')
 
 application = get_asgi_application()
