bc70dedb241914e91efee9bdc22a49c547fa44e7
moohhiit <itzmohitsharma20@gmail.com>
2025-06-22 12:14:02 +0530

    solve the whitenoise modle

--- Server/SecureAPI/settings.py
+++ Server/SecureAPI/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'SecureAPI.urls'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 TEMPLATES = [
     {
