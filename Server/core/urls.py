2587463d1364507fadc7209c6fcf7bea4a82f5b5
Ansh191124 <Mransh1911@gmail.com>
2025-06-26 09:31:14 +0530

    Update Report urls.py

--- Server/core/urls.py
+++ Server/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import register_user, login_user, check_text , message
+from .views import register_user, login_user, check_text , message , Report
 
 urlpatterns = [
     path("register/", register_user),
     path("login/", login_user),
     path("check/", check_text),
-    path("message/" , message)
+    path("message/" , message),
+    path('api/reports/<str:uni_id>/', views.get_reports, name='get_reports'),
     # path('registerapp' , register_app )
 ]
